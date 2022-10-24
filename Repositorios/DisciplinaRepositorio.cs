f33ff6dffe38b33f63f17657b2a6e6c23efee935
Wendley007 <wsanttossttreett@gmail.com>
2022-10-24 15:35:33 -0300

    inicial

--- Repositorios/DisciplinaRepositorio.cs
+++ Repositorios/DisciplinaRepositorio.cs
@@ -1,6 +1,54 @@
+using MangaI.Data;
+using MangaI.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace MangaI.Repositorios;
 
 public class DisciplinaRepositorio
 {
-    
+
+    //Campo que vai ser injetado no construtor
+    private ContextoBD _contexto;
+
+    //Construtor que injeta a dependencia
+    public DisciplinaRepositorio([FromServices] ContextoBD contexto)
+    {
+        _contexto = contexto;
+    }
+
+    public Disciplina CriarDisciplina(Disciplina disciplina)
+    {
+        //Manda o contexto salvar no BD
+        _contexto.Disciplinas.Add(disciplina);
+        _contexto.SaveChanges();
+
+        //Vai estar preenchido com a chave primária
+        return disciplina;
+    }
+
+    public List<Disciplina> ListarDisciplinas()
+    {
+        return _contexto.Disciplinas.ToList();
+    }
+
+    public Disciplina BuscarDisciplinaPeloId(int id)
+    {
+        //Buscar pelo id no contexto
+        return _contexto.Disciplinas.FirstOrDefault(disciplina => disciplina.Id == id);
+    }
+
+    public void RemoverDisciplina(Disciplina disciplina)
+    {
+        //Mandar o contexto remover
+        _contexto.Remove(disciplina);
+        _contexto.SaveChanges();
+
+    }
+
+     public void AtualizarDisciplina()
+  {
+    _contexto.SaveChanges();
+  }
+
+
 }
--- Services/DisciplinaServico.cs
+++ Services/DisciplinaServico.cs
@@ -1,6 +1,32 @@
+using MangaI.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+
 namespace MangaI.Services;
 
 public class DisciplinaServico
 {
-    
+    //Campo que é injetado no construtor
+    private DisciplinaRepositorio _disciplinaRepositorio;
+
+    //Construtor com injecao de dependencia
+    public DisciplinaServico([FromServices] DisciplinaRepositorio repositorio)
+    {
+        _disciplinaRepositorio = repositorio;
+    }
+
+    public void RemoverDisciplina(int id)
+  {
+    //Busar o procedimento (modelo) pelo id
+    var disciplina = _disciplinaRepositorio.BuscarDisciplinaPeloId(id);
+
+    if (disciplina is null)
+    {
+      return; //no futuro vai ser uma exceção
+    }
+
+    //Mandar o repositorio remover o modelo
+    _disciplinaRepositorio.RemoverDisciplina(disciplina);
+  }
+
+
 }
