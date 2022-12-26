0ddb2883b25ce024b33e477f4429e2fe3ac3ff18
LucianoASF <lucianoaugustos@gmail.com>
2022-12-26 13:55:54 -0300

    L

--- Dtos/UsuarioCriarRequisicao.cs
+++ Dtos/UsuarioCriarRequisicao.cs
@@ -7,6 +7,8 @@ public class UsuarioCriarRequisicao
     public string Nome { get; set; }
     public string Email { get; set; }
     public string CPF { get; set; }
+
+    public string Senha { get; set; }
     public string RG { get; set; }
     public DateTime DataNascimento { get; set; }
     public string Sexo { get; set; }
